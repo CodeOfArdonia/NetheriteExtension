481ea6a1bf6bdd34ba8c0f977307d60bc86bbad5
IAFEnvoy <tanksheng545@126.com>
2025-05-16 14:24:25 +0800

    fix conflict with cape

---
+++ common/src/main/java/com/iafenvoy/netherite/mixin/CapeFeatureRendererMixin.java
@@ -0,0 +1,20 @@
+package com.iafenvoy.netherite.mixin;
+
+import com.iafenvoy.netherite.registry.NetheriteItems;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.feature.CapeFeatureRenderer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Environment(EnvType.CLIENT)
+@Mixin(CapeFeatureRenderer.class)
+public abstract class CapeFeatureRendererMixin {
+    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/network/AbstractClientPlayerEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    private boolean allowElytra(boolean original, @Local ItemStack itemStack) {
+        return original || itemStack.isOf(NetheriteItems.NETHERITE_ELYTRA.get());
+    }
+}
