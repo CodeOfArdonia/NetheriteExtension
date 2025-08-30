62083bf4052e2074687580624b6e6be0730ef94a
IAFEnvoy <tanksheng545@126.com>
2025-08-30 19:07:07 +0800

    jij arch, fix elytra in forge

--- forge/src/main/java/com/iafenvoy/netherite/item/impl/forge/NetheriteElytraItemImpl.java
+++ forge/src/main/java/com/iafenvoy/netherite/item/impl/forge/NetheriteElytraItemImpl.java
@@ -1,9 +1,11 @@
 package com.iafenvoy.netherite.item.impl.forge;
 
 import com.iafenvoy.netherite.item.impl.NetheriteElytraItem;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.event.GameEvent;
 
 public class NetheriteElytraItemImpl extends NetheriteElytraItem {
     protected NetheriteElytraItemImpl(Settings settings) {
@@ -17,5 +19,18 @@ public class NetheriteElytraItemImpl extends NetheriteElytraItem {
     @Override
     public boolean canElytraFly(ItemStack stack, LivingEntity entity) {
         return ElytraItem.isUsable(stack);
+    }
+
+    @Override
+    public boolean elytraFlightTick(ItemStack stack, LivingEntity entity, int flightTicks) {
+        if (!entity.getWorld().isClient) {
+            int nextFlightTick = flightTicks + 1;
+            if (nextFlightTick % 10 == 0) {
+                if (nextFlightTick % 20 == 0)
+                    stack.damage(1, entity, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.CHEST));
+                entity.emitGameEvent(GameEvent.ELYTRA_GLIDE);
+            }
+        }
+        return true;
     }
 }
