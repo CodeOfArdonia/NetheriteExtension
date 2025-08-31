624fd41f69d956084c31325d1b8313c1d8372d06
IAFEnvoy <tanksheng545@126.com>
2025-08-31 14:00:58 +0800

    remove unused queue

--- common/src/main/java/com/iafenvoy/netherite/NetheriteExtensionClient.java
+++ common/src/main/java/com/iafenvoy/netherite/NetheriteExtensionClient.java
@@ -2,15 +2,11 @@ package com.iafenvoy.netherite;
 
 import com.iafenvoy.netherite.registry.NetheriteRenderers;
 import com.iafenvoy.netherite.registry.NetheriteScreenHandlers;
-import it.unimi.dsi.fastutil.ints.IntArraySet;
-import it.unimi.dsi.fastutil.ints.IntSet;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
 public class NetheriteExtensionClient {
-    public static final IntSet NETHERITE_TRIDENT_LIST = new IntArraySet();
-
     public static void init() {
         NetheriteRenderers.registerEntityRenderers();
     }
